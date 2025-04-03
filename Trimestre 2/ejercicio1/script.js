e6c0185868abda57fbc7dc3336d0862892b918e0
Mario0351 <mmanrom2807@g.educaand.es>
2025-04-03 08:54:42 +0200

    ejercicio presentacion

---
+++ Trimestre 2/ejercicio1/script.js
@@ -0,0 +1,27 @@
+const profileImage = document.getElementById('profileImage');
+
+profileImage.addEventListener('mouseover', () => {
+    profileImage.style.boxShadow = '0 0px 1000px rgba(246, 20, 20, 0.5)';
+    profileImage.src = "https://img2.rtve.es/i/?w=1600&i=1673113417228.jpg";
+});
+profileImage.addEventListener('mouseout', () => {
+    profileImage.style.boxShadow = 'none';
+    profileImage.src = "https://img.a.transfermarkt.technology/portrait/big/69751-1710080407.jpg?lm=1";
+})
+
+var ChangeNameColorButton = document.getElementById('ChangeNameColor');
+var fullNameH1 = document.getElementById('fullName');
+
+ChangeNameColorButton.addEventListener('click', function() {
+    console.warn("click........");
+    alert("Ahora el nombre va a cambiar de color");
+    fullNameH1.style = "color: red; font-style: italic;";
+});
+
+//nameColorButton.addEventListener('click', () => {
+//     const titleName = document.getElementById('fullname');
+//     if (titleName.style.color == '') {
+//         titleName.style.color = 'blue';
+//     }else{
+//         titleName.style.color = '';
+//     }
